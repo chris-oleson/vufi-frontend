39d9c0a5c7c1b3ed937b99d21759c08d895f6e23
fishCROjr <crolesonjr@gmail.com>
2023-05-07 12:12:06 -0600

    almost there

--- api/test.js
+++ api/test.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/api/test', (req, res) => {
+  res.status(200).send('Test endpoint');
 });
 
 module.exports = app;

ea9a2b32beff8e9ab6090931a00f0973f71156e9
fishCROjr <crolesonjr@gmail.com>
2023-05-07 10:44:27 -0600

    testing serverless functions

---
+++ api/createNewUser.js
@@ -0,0 +1,28 @@
+const { Router } = require('express')
+const router = Router()
+const bcrypt = require('bcryptjs')
+const db = require('./database')
+
+// Create new user
+router.post('/create', (req, res) => {
+    // Check if there is a matching existing email
+    db.query("SELECT * FROM users WHERE email = ?", [req.body.email], (err, results) => {
+        if (results.length) {
+            res.sendStatus(409)
+        }
+        else {
+            // Encrypt new password
+            let salt = bcrypt.genSaltSync(10)
+            let encryptedPassword = bcrypt.hashSync(req.body.password, salt)
+            
+            // Add user data
+            db.query("INSERT INTO users VALUES (null, ?, ?, ?, ?)", [req.body.email, encryptedPassword, req.body.firstName, req.body.lastName], (err, results) => {
+                    res.send(results.insertId.toString())
+
+                    // Add default user prefs
+                    db.query("INSERT INTO user_prefs VALUES (null, 0, 'USD', ?)", [results.insertId])
+                }
+            )
+        }
+    })
+})

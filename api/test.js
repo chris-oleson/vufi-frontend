c00e407605ff66304c2dce9566e37b19872ed536
fishCROjr <crolesonjr@gmail.com>
2023-05-07 12:19:25 -0600

    testing db connection

--- api/test.js
+++ api/test.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2')
 
-app.get('/api/test', (req, res) => {
-  res.status(200).send('Test endpoint');
-});
+mysql.createConnection({
+    host: process.env.PLANETSCALE_DB_HOST,
+    user: process.env.PLANETSCALE_DB_USERNAME,
+    password: process.env.PLANETSCALE_DB_PASSWORD,
+    database: process.env.PLANETSCALE_DB
+})
+
+app.get('/api/users', (req, res) => {
+    db.query("SELECT * FROM users WHERE email = ?", [req.body.email], (err, results) => {
+        res.status(200).send(results)
+    })
+})
 
 module.exports = app;

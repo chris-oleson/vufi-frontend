4f463d8619faa6e5aa396611b5c832efeded3a57
fishCROjr <crolesonjr@gmail.com>
2023-05-07 12:59:24 -0600

    fixing error

--- api/users.js
+++ api/users.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql2')
-const db = mysql.createConnection(process.env.DATABASE_URL)
+const connection = mysql.createConnection(process.env.DATABASE_URL)
 
 app.get('/api/users', (req, res) => {
-    db.query("SELECT * FROM users", (err, results) => {
+    connection.query("SELECT * FROM users", (err, results) => {
         res.status(200).send(results)
         connection.end()
     })

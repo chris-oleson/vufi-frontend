3c4eae70718e6318cdf9b647e074ebad30d8bda7
Chris Oleson <crolesonjr@gmail.com>
2023-10-29 12:40:36 -0600

    Fixing small bugs

--- src/views/CancelSubscription.vue
+++ src/views/CancelSubscription.vue
@@ -2,7 +2,7 @@
     <v-card class="pa-10 mx-auto mt-10 text-center" width="330">
         <img src="/logo.svg" height="50" width="50" class="mx-auto"/>
 
-        <v-text-field variant="underlined" class="my-4" label="Current Password" :error="incorrectPassword" type="password" v-model="password" @keyup.enter="deleteAccount"></v-text-field>
+        <v-text-field variant="underlined" class="my-4" label="Current Password" :error="incorrectPassword" type="password" v-model="password" @keyup.enter="cancelSubscription"></v-text-field>
         <v-card-text v-if="incorrectPassword" class="text-error pa-0">Incorrect password</v-card-text>
 
         <v-btn rounded="0" class="bg-error mt-4" width="200" @click="cancelSubscription">Cancel Subscription</v-btn>
@@ -11,7 +11,7 @@
 
 <script>
 export default {
-    name: 'vufi-delete-account',
+    name: 'vufi-cancel-subscription',
 
     data() {
         return {

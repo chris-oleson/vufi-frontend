1af5c534424037cd040506e73ee02946bfe89447
Chris Oleson <crolesonjr@gmail.com>
2023-11-13 13:32:34 -0700

    Improving change password interface

--- src/router.js
+++ src/router.js
@@ -130,7 +130,6 @@ export default createRouter({
         },
         {
             path: '/change-password',
-            beforeEnter: rejectNoParams,
             component: ChangePassword,
             meta: {
                 title: 'Change Password - VuFi'
